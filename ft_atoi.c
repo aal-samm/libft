3c2ae5c37993aaeffc13f2408b7c4a77ec633e92
Sam <anwar_samman@yahoo.com>
2023-11-06 02:11:31 +0100

    before co

--- ft_atoi.c
+++ ft_atoi.c
@@ -3,11 +3,48 @@
 /*                                                        :::      ::::::::   */
 /*   ft_atoi.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aal-samm <aal-samm@student.42.fr>          +#+  +:+       +#+        */
+/*   By: abuelnoor <abuelnoor@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/01 16:22:11 by aal-samm          #+#    #+#             */
-/*   Updated: 2023/11/01 16:31:25 by aal-samm         ###   ########.fr       */
+/*   Updated: 2023/11/06 01:29:34 by abuelnoor        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#inclue 
+#include "libft.h"
+
+int ft_atoi(const char *nptr)
+{
+	int sign;
+	int i;
+	int result;
+
+	i = 0;
+	result = 0;
+	while (nptr[i] != '\0' && (nptr[i] == ' ' ||  (nptr[i] >= 9 && nptr[i] <= 13)))
+		i++;
+	if (nptr[i] == '-')
+		{
+			sign = -1;
+			i++;
+		} else if (nptr[i] == '+')
+		{
+			sign = 1;
+			i++;
+		}
+	while (nptr[i] != '\0' && (nptr[i] >= '0' && nptr[i] <= '9'))
+	{
+		result = result * 10 + (nptr[i] - '0');
+		i++;
+	}
+	return (result * sign);
+}
+
+/*#include <stdio.h>
+int main (){
+	char pr[] = "-2+-23fwe12";
+	int i = atoi (pr);
+	int sec = ft_atoi(pr);
+	printf("expected:%d \n", i);
+	printf("my result is :%d \n", sec);
+	return (0);
+}*/
---
+++ ft_strtrim.c
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abuelnoor <abuelnoor@student.42.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/04 21:46:51 by abuelnoor         #+#    #+#             */
+/*   Updated: 2023/11/05 03:01:52 by abuelnoor        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int ft_check(const char *set, char c)
+{
+	while (*set)
+	{
+		if (c == *set)
+			return (1);
+		set++;
+	}
+	return (0);
+}
+char *ft_strtrim(char const *s1, char const *set)
+{
+	size_t i;
+	size_t start;
+	size_t end;
+	char * n_str;
+	
+	if (!s1 || !set)
+		return (NULL);
+	start = 0;
+	end = ft_strlen(s1);
+	while (s1[start] && ft_check(set, s1[start]))
+		start++;
+	while (end > start && ft_check(set, s1[end - 1]))
+		end--;
+	n_str = (char *)malloc(end - start + 1);
+	if(!n_str)
+		return (NULL);
+	i = 0;
+	while (s1[start] &&  start < end)
+	{
+		n_str[i] = s1[start];
+		i++;
+		start++;
+	}
+	n_str[i] = '\0';
+	return (n_str);
+}
+/*#include <stdio.h>
+int main ()
+{
+	char *ptr = "+-are +- you ok daddy*-+f";
+	char *see = "+-*";
+	char *result = ft_strtrim(ptr, see);
+	printf("first %s \n", ptr);
+	printf("second %s \n", result);
+	return (0);
+}*/
