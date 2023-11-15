52de14c473b1dda2cd038c3613084c96345ecf5f
Anwar Al Samman <aal-samm@1-B-8.42heilbronn.de>
2023-11-15 20:03:51 +0100

    mf1

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: aal-samm <aal-samm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/14 23:18:07 by aal-samm          #+#    #+#             */
-/*   Updated: 2023/11/14 23:51:35 by aal-samm         ###   ########.fr       */
+/*   Updated: 2023/11/15 19:48:32 by aal-samm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,25 @@
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	
-}
+	void	*p;
+	t_list	*new_nod;
+	t_list	*new_lst;
 
+	if (!lst || !f || !del)
+		return (NULL);
+	new_lst = NULL;
+	while (lst)
+	{
+		p = f(lst->content);
+		new_nod = ft_lstnew(p);
+		if (!new_nod)
+		{
+			del(p);
+			ft_lstclear (&new_lst, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&new_lst, new_nod);
+		lst = lst->next;
+	}
+	return (new_lst);
+}
